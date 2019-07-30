b264e3c403e4561be3c083f285a95e310e655934
SparkAngel <igor.gnatenko555@gmail.com>
2019-07-30 14:46:31 +0300

    Updates

---
+++ precache-manifest.93f3e2334f86b3087f63aefa58912687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9aae69e8f3cc38e973184175b2cc70e",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "980b861eab9209861b1b",
+    "url": "/react_tabs/static/css/main.1e9b8d30.chunk.css"
+  },
+  {
+    "revision": "ffbe32d4e790e6a5d2f2",
+    "url": "/react_tabs/static/js/2.335f0c66.chunk.js"
+  },
+  {
+    "revision": "980b861eab9209861b1b",
+    "url": "/react_tabs/static/js/main.4131d871.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
